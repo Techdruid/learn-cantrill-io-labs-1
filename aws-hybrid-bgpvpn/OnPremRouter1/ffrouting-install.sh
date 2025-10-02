5a621a4c05065728cfb8c26d998a1eb221ebf897
Simone Urbinati <Techdruid@users.noreply.github.com>
2025-10-02 13:44:30 +0200

    Update ffrouting-install.sh

--- aws-hybrid-bgpvpn/OnPremRouter1/ffrouting-install.sh
+++ aws-hybrid-bgpvpn/OnPremRouter1/ffrouting-install.sh
@@ -33,7 +33,7 @@ sudo apt-get install libssh-dev -y
 cd /tmp
 git clone -b 0.7.0 https://github.com/rtrlib/rtrlib/ rtrlib
 cd rtrlib
-git checkout 0.7.0   # or a commit before ASPA refactor
+git checkout tags/v0.6.3 -b rtrlib-0.6.3
 mkdir build; cd build 
 cmake -D CMAKE_BUILD_TYPE=Release ..
 make
